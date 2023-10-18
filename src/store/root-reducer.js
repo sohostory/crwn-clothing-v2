65f213deb3c013b1b439af1e040323059112dfcc
sohostory <sohostory@gmail.com>
2023-10-17 20:40:44 -0500

    setting up user reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -3,6 +3,5 @@ import { combineReducers } from "redux";
 import { userReducer } from "./user/user.reducer";
 
 export const rootReducer = combineReducers({
-  // name of reducer slice and reducer function
   user: userReducer,
 });
